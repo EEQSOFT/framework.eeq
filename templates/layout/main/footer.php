0eb5488739dfdd5e960764a9d1d93f48dce7066c
WEBEEQ-DESKTOP\Robert <webeeq@gmail.com>
2024-12-06 17:29:51 +0100

    Change footer.php file

--- templates/layout/main/footer.php
+++ templates/layout/main/footer.php
@@ -1,1 +1,1 @@
-by EEQSOFT
+<p class="info">Powered by <a href="https://phpframework.eeqsoft.com/" title="PHP Framework from EEQSOFT">PHP Framework</a> from <a href="https://www.eeqsoft.com/">EEQSOFT</a></p>
